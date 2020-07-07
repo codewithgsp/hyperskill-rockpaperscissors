f2a1a9e6830e353c348a884839a87071ee10dbd5
codewithgsp <codewithgsp@gmail.com>
2020-07-07 04:30:29 +0200

    Stage - 3 RPS

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -7,7 +7,8 @@ class RockPaperScissors:
                                  'draw': 'There is a draw ({})',
                                  'lose': 'Sorry, but computer chose {}'}
 
-    options = ['rock', 'paper', 'scissors']
+    computer_options = ['rock', 'paper', 'scissors']
+    game_valid_words = set(computer_options + ['!exit'])
 
     win_scenario = {'rock': 'scissors',
                     'paper': 'rock',
@@ -21,7 +22,7 @@ class RockPaperScissors:
         print(self.result_to_display_mapping.get(result).replace('{}', self.computer_option))
 
     def determine_option(self):
-        self.computer_option = choice(self.options)
+        self.computer_option = choice(self.computer_options)
         if self.option == self.computer_option:
             return 'draw'
         elif self.win_scenario.get(self.option) == self.computer_option:
@@ -29,10 +30,22 @@ class RockPaperScissors:
         else:
             return 'lose'
 
+    def determine_valid_words(self):
+        if self.option not in self.game_valid_words:
+            print('Invalid input')
+            return False
+        if self.option == '!exit':
+            print('Bye!')
+            exit(0)
+        return True
+
     def play(self):
-        result = self.determine_option()
-        self.display_result(result)
+        if self.determine_valid_words():
+            result = self.determine_option()
+            self.display_result(result)
 
 
-rps = RockPaperScissors(input())
-rps.play()
+while True:
+    user_choice = input()
+    rps = RockPaperScissors(user_choice)
+    rps.play()
